087c4d6320a24c2b40274ceb9c3f6cd30ce0ff57
Archu-1234 <ashwiniarchana399@gmail.com>
2024-02-22 15:27:33 +0530

    test

---
+++ Test.java
@@ -0,0 +1,17 @@
+
+class Test {
+    public static void main(String[] args) {
+      
+      String s3= new String("Afternoon");
+      String s1="Hello";
+      String s2="Gm";
+
+        System.out.println(s3=s1); 
+    }
+}
+
+
+
+
+        
+         
