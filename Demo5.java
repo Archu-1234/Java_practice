113bee62262c31671aa912642e9f5eaead16f8cb
Archu-1234 <ashwiniarchana399@gmail.com>
2024-02-24 11:26:17 +0530

    hello

---
+++ Demo5.java
@@ -0,0 +1,22 @@
+interface Carrot{
+    public void good();
+    public void bad();
+}
+class  D implements Carrot{
+    public void good(){
+        // system.out.println("hii");
+        System.out.println("hii");
+    }
+    public void bad(){
+    //   system.out.println("hello");
+    System.out.println("hello");
+    
+    }
+}
+class Demo5{
+    public static void main(String[] args){
+        D s=new D();
+        s.good();
+        s.bad();
+    }
+}
