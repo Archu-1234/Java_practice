85e95140437717117de5437d8101978da7c8a44b
Archu-1234 <ashwiniarchana399@gmail.com>
2024-02-27 10:49:32 +0530

    print

---
+++ TestFive.java
@@ -0,0 +1,15 @@
+class Test{
+	public static void main(String[] args){
+		
+		//array declartion
+		int []ids;
+		
+		//array creation
+		char a='A';
+		int size = 100;
+		ids=new int['b'];
+		
+		System.out.println(ids.length);
+	}
+
+}
---
+++ TestFour.java
@@ -0,0 +1,13 @@
+Class Test{
+    public static void main(String[] args){
+             //array declaration
+             int []ids;
+
+             //array creation
+             char a='A';
+             int size = 100;
+             ids=new int['b'];
+
+             System.out.println(ids.length);
+    }
+}
